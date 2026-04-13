0aa9eebc10fd32c3af5c7edb1beca40885117807
HafizRahmiyanto <karinrahmiyanti@gmail.com>
2026-04-13 08:29:38 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -1,5 +1,44 @@
 #include <stdio.h>
 
-int main(void) {
+void gerbang(int izin, int radiasi, int jam, float suhu){
+
+    if(radiasi >= 6){
+        printf("TOLAK\n");
+    }
+    else if(suhu >= 390){
+        printf("KARANTINA\n");
+    }
+    else if(izin == 1 && (jam < 6 || jam > 20)){
+        printf("TOLAK\n");
+    }
+    else if(izin == 1){
+        printf("MASUK\n");
+    }
+    else if(izin == 2 && radiasi <= 2 && (jam >= 8 || jam <= 18)){
+        printf("MASUK\n");
+    }
+    else if(izin == 2){
+        printf("PEMERIKSAAN\n");
+    }
+    else if(izin == 3 && radiasi == 0 && suhu < 380){
+        printf("MASUK\n");
+    }
+    else{
+        printf("TOLAK\n");
+    }
+    return;
+}
+
+int main(){
+    int izin, radiasi, jam;
+    float suhu;
+
+    scanf("%d", &izin);
+    scanf("%f", &suhu);
+    scanf("%d", &radiasi);
+    scanf("%d", &jam);
+
+    gerbang(izin, radiasi, jam, suhu);
+
     return 0;
 }
